6a38500bec0c0138f1c6cc2fb3d033157e974dcd
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-07-02 12:59:14 -0400

    workshop 3

--- authenticate.js
+++ authenticate.js
@@ -1,7 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
-
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
 const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
@@ -39,3 +38,13 @@ exports.jwtPassport = passport.use(
 );
 
 exports.verifyUser = passport.authenticate('jwt', {session: false});
+
+exports.verifyAdmin = function(req, res, next) {
+    if (req.user.admin === true) {
+        return next();
+    } else {
+        const err = new Error('You are not authorized to perform this operation!');
+        res.statusCode = 403;
+        return next(err);
+    }
+}
