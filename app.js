0fa78e4809c2644a7db77de72abaea92e39a1752
Abid Ahmed <abid.ahmedaccessor3@gmail.com>
2022-07-10 07:28:21 -0400

    workshop 4

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const session = require('express-session');
-const FileStore = require('session-file-store')(session);
+const passport = require('passport');
+const config = require('./config');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 const campsiteRouter = require('./routes/campsiteRouter');
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 const uploadRouter = require('./routes/uploadRouter');
+const favoriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
-const passport = require('passport');
-const config = require('./config');
 
-const authenticate = require('./authenticate');
 const url = config.mongoUrl;
 const connect = mongoose.connect(url, {
     useCreateIndex: true,
     useFindAndModify: false,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
 });
 
-connect.then(() => console.log('Connected correctly to server'),
+connect.then(
+    () => console.log('Connected correctly to server'),
     err => console.log(err)
 );
 
@@ -35,12 +32,19 @@ var app = express();
 
 // Secure traffic only
 app.all('*', (req, res, next) => {
-  if (req.secure) {
-    return next();
-  } else {
-      console.log(`Redirecting to: https://${req.hostname}:${app.get('secPort')}${req.url}`);
-      res.redirect(301, `https://${req.hostname}:${app.get('secPort')}${req.url}`);
-  }
+    if (req.secure) {
+        return next();
+    } else {
+        console.log(
+            `Redirecting to: https://${req.hostname}: ${app.get('secPort')}${
+                req.url
+            }`
+        );
+        res.redirect(
+            307,
+            `https://${req.hostname}:${app.get('secPort')}${req.url}`
+        );
+    }
 });
 
 // view engine setup
@@ -50,58 +54,35 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser('12345-67890-09876-54321'));
-
-app.use(session({
-  name: 'session-id',
-  secret: '12345-67890-09876-54321',
-  saveUninitialized: false,
-  resave: false,
-  store: new FileStore()
-}));
+//app.use(cookieParser('12345-67890-09876-54321'));
 
 app.use(passport.initialize());
-app.use(passport.session());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-function auth(req, res, next) {
-    console.log(req.user);
-
-    if (!req.user) {
-        const err = new Error('You are not authenticated!');
-        err.status = 401;
-        return next(err);
-    } else {
-        return next();
-    }
-}
-
-app.use(auth);
-
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
 app.use('/imageUpload', uploadRouter);
+app.use('/favorites', favoriteRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
--- authenticate.js
+++ authenticate.js
@@ -2,8 +2,9 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;  //provides helper methods such as extracting Jw token from a request object
-const jwt = require('jsonwebtoken');   //node modules used to create/sign/verify tokens
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 const config = require('./config.js');
 
@@ -11,41 +12,70 @@ exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-exports.getToken = user => {
-    return jwt.sign(user, config.secretKey, {expiresIn: 3600});
+exports.getToken = function (user) {
+    return jwt.sign(user, config.secretKey, { expiresIn: 3600 });
 };
 
 const opts = {};
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();  //token is to be extracted from authorization header
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
 opts.secretOrKey = config.secretKey;
 
 exports.jwtPassport = passport.use(
-    new JwtStrategy(
-        opts,
-        //function verify(jwt_payload, done)
-        (jwt_payload, done) => {
-            console.log('JWT payload:', jwt_payload);
-            User.findOne({_id: jwt_payload._id}, (err, user) => {
+    new JwtStrategy(opts, (jwt_payload, done) => {
+        console.log('JWT payload:', jwt_payload);
+        User.findOne({ _id: jwt_payload._id }, (err, user) => {
+            if (err) {
+                return done(err, false);
+            } else if (user) {
+                return done(null, user);
+            } else {
+                return done(null, false);
+            }
+        });
+    })
+);
+
+exports.verifyUser = passport.authenticate('jwt', { session: false });
+
+exports.verifyAdmin = (req, res, next) => {
+    if (req.user.admin) {
+        next();
+    } else {
+        const err = new Error(
+            'You are not authorized to perform this operation!'
+        );
+        err.status = 403;
+        next(err);
+    }
+};
+
+exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy(
+        {
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret,
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({ facebookId: profile.id }, (err, user) => {
                 if (err) {
                     return done(err, false);
-                } else if (user) {
-                    return done(null, user);  //passport uses done callback to access the user document and load the info to request object (done from passport-jwt module, it is written for us)
+                }
+                if (!err && user !== null) {
+                    return done(null, user);
                 } else {
-                    return done(null, false);  //when no error, but also no user was found
+                    user = new User({ username: profile.displayName });
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.save((err, user) => {
+                        if (err) {
+                            return done(err, false);
+                        } else {
+                            return done(null, user);
+                        }
+                    });
                 }
             });
         }
     )
 );
-
-exports.verifyUser = passport.authenticate('jwt', {session: false});
-
-exports.verifyAdmin = function(req, res, next) {
-    if (req.user.admin === true) {
-        return next();
-    } else {
-        const err = new Error('You are not authorized to perform this operation!');
-        res.statusCode = 403;
-        return next(err);
-    }
-}
